653482ea4f11e0100cb8d96d87c0150fdb586491
Thiago Pereira <thiago.spereira02@gmail.com>
2026-03-13 13:14:41 +0000

    feat: create VeiculoController to expose vehicles to the frontend

---
+++ backend/src/main/java/com/logap/logitrack/controller/VeiculoController.java
@@ -0,0 +1,21 @@
+package com.logap.logitrack.controller;
+
+import com.logap.logitrack.entity.Veiculo; 
+import com.logap.logitrack.repository.VeiculoRepository; 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/veiculos")
+public class VeiculoController {
+
+    @Autowired
+    private VeiculoRepository veiculoRepository;
+
+    @GetMapping
+    public List<Veiculo> listarTodos() {
+        return veiculoRepository.findAll();
+    }
+}
